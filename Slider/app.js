aa476614d42d2e2607694ddf15f70b65cadb0481
aryan0k9 <84584531+aryan0k9@users.noreply.github.com>
2022-07-10 16:57:47 +0530

    Add files via upload

---
+++ Slider/app.js
@@ -0,0 +1,27 @@
+let flag =0;
+
+function controller(x){
+    flag = flag + x;
+    slideshow(flag);
+}
+
+slideshow(flag);
+function slideshow(num){
+    let image = document.getElementsByClassName('slide');
+    if(num==image.length){
+        flag=0;
+        num=0;
+    }
+    if(num < 0){
+        flag=image.length-1;
+        num=image.length-1;
+    }
+    for(let y of image){
+        y.style.display = 'none';
+    }
+    image[num].style.display = 'block';
+    console.log(image);
+
+}
+
+
